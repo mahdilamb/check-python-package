313ba871882d5a767d8d88645377edb2d6c0df69
Mahdi Lamb <mahdilamb@gmail.com>
2023-08-18 23:16:39 +0100

    :

--- scripts/generate_actions.py
+++ scripts/generate_actions.py
@@ -21,7 +21,7 @@ DATA = {
             },
             {
                 "name": "Run package checker",
-                "run": """export PYTHONPATH=${{ github.action_path }}:$PYTHONPATH && pip install pyyaml && python -m package_checker --default-branch=${{ github.event.repository.default_branch }} --current-branch=${{ github.ref_name }} --action-yaml=${{ github.action_path }}/action.yaml """
+                "run": """export PYTHONPATH=${{ github.action_path }}:$PYTHONPATH && pip install ${{ github.action_path }} && python -m package_checker --default-branch=${{ github.event.repository.default_branch }} --current-branch=${{ github.ref_name }} --action-yaml=${{ github.action_path }}/action.yaml """
                 + f"{INPUTS:cli_args}",
                 "shell": "bash",
             },
