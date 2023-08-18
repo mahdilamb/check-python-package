32f1e911716142204eb103031522959e9abcef8f
Mahdi Lamb <mahdilamb@gmail.com>
2023-08-18 03:48:20 +0100

    debug

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -2,15 +2,15 @@ import subprocess, re, pkg_resources, sys
 
 
 path, variable, default_branch = sys.argv[1:4]
-print(subprocess.run(f"git show {default_branch}:{path}", capture_output=True))
+print(subprocess.check_output(["git", "show", f"{default_branch}:{path}"]))
 version_pattern = re.compile(rf"^{variable}.*?=.*?[''\"](.*?)[''\"]", flags=re.M)
 to_version = lambda cmd: pkg_resources.parse_version(
     version_pattern.findall(
-        subprocess.run(cmd, capture_output=True, shell=True).stdout.decode()
+        subprocess.check_output(cmd).decode()
     )[0]
 )
 main = to_version(f"git show main:{path}")
-current = to_version(f"cat {path}")
+current = to_version(["cat",path])
 assert (
     main < current
 ), f"Version of current commit ({current}) has not been incremented (from {main})."
