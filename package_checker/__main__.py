302037548b9dd6f8c09bc639141395e308af0085
Mahdi Lamb <mahdilamb@gmail.com>
2023-08-18 16:18:38 +0100

    :

--- package_checker/__main__.py
+++ package_checker/__main__.py
@@ -5,9 +5,11 @@ import subprocess
 import sys
 
 from package_checker import _api
+import yaml
+import argparse
 
 PACKAGE_ROOT = os.path.normpath(os.path.join(__file__, "..", ".."))
-
+package_info = _api.PackageInfo()
 
 def import_module(path: str):
     relative_path = os.path.relpath(path, PACKAGE_ROOT)
@@ -17,10 +19,18 @@ def import_module(path: str):
     sys.modules[module_path] = module
     spec.loader.exec_module(module)
     return module
-import pprint
-for name, value in os.environ.items():
-    print("{0}: {1}".format(name, value))
-package_info = _api.PackageInfo()
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--default-branch")
+    parser.add_argument("--current-branch")
+    with open("action.yaml", "rb") as fp:
+        for _in,__ in yaml.safe_load(fp).get("inputs",[]).items():
+            parser.add_argument(f"--{_in}", required=__.get("required", False), default=__.get("default"), help=__.get("description"))
+    return parser
+parser = create_parser()
+args = parser.parse_args()
+print(vars(args))
 for file in glob.glob(os.path.join(PACKAGE_ROOT, "*", "*.py")):
     if os.path.basename(file).startswith("_"):
         continue
