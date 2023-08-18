b0b4e93a657195484fd2f2ded51a785d4137fda2
Mahdi Lamb <mahdilamb@gmail.com>
2023-08-18 16:39:57 +0100

    :

--- package_checker/__main__.py
+++ package_checker/__main__.py
@@ -29,7 +29,7 @@ def create_parser():
     return parser
 parser = create_parser()
 args = parser.parse_args()
-
+print(args)
 package_info = _api.PackageInfo(**{k:v for k,v in vars(args).items() if v in {None,"-"}})
 if package_info.current_branch == package_info.default_branch:
     exit(0)
