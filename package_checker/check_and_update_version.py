426b2419350cef6da3a014613998e4410369ab5d
Mahdi Lamb <mahdilamb@gmail.com>
2023-08-18 17:24:57 +0100

    :

--- package_checker/check_and_update_version.py
+++ package_checker/check_and_update_version.py
@@ -11,8 +11,8 @@ def required(info):
 
 
 def run(info: _api.PackageInfo):
-    subprocess.check_output(["git","checkout",info.default_branch ])
-    subprocess.check_output(["git","checkout",info.current_branch ])
+    subprocess.check_output(["git", "checkout", info.default_branch])
+    subprocess.check_output(["git", "checkout", info.current_branch])
     version_pattern = re.compile(
         rf"^({info.version_check_variable}.*?=.*?['\"])(.*?)(['\"].*)$", flags=re.M
     )
@@ -24,23 +24,13 @@ def run(info: _api.PackageInfo):
     )
     current = to_version(["cat", info.version_check_path])
     next_version = re.sub(r"^(\d+\.\d+)\.\d*(.*)$", rf"\1.{main.micro+1}\2", str(main))
-    print(main,current,next_version)
-    assert (
-        main < current
-    ), f"Version of current commit ({current}) has not been incremented (from {main})."
-
-
-def update_version():
-
-
-    main = to_version(["git", "show", f"{default_branch}:{path}"])
-    
-    with open(path, "r") as fp:
-        text = fp.read()
-        with open(path, "w") as fp:
-            fp.write(
-                version_pattern.sub(
-                    rf"\g<1>{next_version}\g<3>",
-                    text,
+    if main < current:
+        with open(info.version_check_path, "r") as fp:
+            text = fp.read()
+            with open(info.version_check_path, "w") as fp:
+                fp.write(
+                    version_pattern.sub(
+                        rf"\g<1>{next_version}\g<3>",
+                        text,
+                    )
                 )
-            )
