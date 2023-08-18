732e4ee6481a5bb16ad0944a572adca3ba761f80
Mahdi Lamb <mahdilamb@gmail.com>
2023-08-18 03:55:32 +0100

    debug

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -2,14 +2,14 @@ import subprocess, re, pkg_resources, sys
 
 
 path, variable, default_branch = sys.argv[1:4]
-print(subprocess.check_output(["git", "show", f"{default_branch}:{path}"]))
+
 version_pattern = re.compile(rf"^{variable}.*?=.*?[''\"](.*?)[''\"]", flags=re.M)
 to_version = lambda cmd: pkg_resources.parse_version(
     version_pattern.findall(
         subprocess.check_output(cmd).decode()
     )[0]
 )
-main = to_version(f"git show main:{path}")
+main = to_version(["git", "show", f"{default_branch}:{path}"])
 current = to_version(["cat",path])
 assert (
     main < current
