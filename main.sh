0897c20bcfaddf743cea576b64c0fc9347d3e65f
Mahdi Lamb <mahdilamb@gmail.com>
2023-08-18 14:41:05 +0100

    move script to bash

---
+++ main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SCRIPT_PATH="$(dirname "$(readlink -fm "$0")")"
+
+VERSION_PATH=$1
+VERSION_VARIABLE=$2
+DEFAULT_BRANCH=$3
+CURRENT_BRANCH=$4
+
+git fetch
+git checkout $DEFAULT_BRANCH
+git checkout $CURRENT_BRANCH
+
+REQUIRES_CHANGE=$(python $SCRIPT_PATH/scripts/check_version.py $VERSION_PATH $VERSION_VARIABLE $DEFAULT_BRANCH)
+if [ $? ]; then
+    echo "Updating version number"
+    python $SCRIPT_PATH/scripts/next_version.py $VERSION_PATH} $VERSION_VARIABLE $DEFAULT_BRANCH
+    1 git config user.name github-actions
+    git config user.email github-actions@github.com
+    git add $VERSION_PATH
+    git commit -m "Update micro version"
+    git push --set-upstream origin $CURRENT_BRANCH
+
+fi
