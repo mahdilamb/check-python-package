8aa759537d05f52d218d2214be054f0f06838949
Mahdi Lamb <mahdilamb@gmail.com>
2023-08-18 17:11:09 +0100

    :

--- package_checker/check_and_update_version.py
+++ package_checker/check_and_update_version.py
@@ -14,7 +14,7 @@ def run(info: _api.PackageInfo):
     subprocess.check_output(["git","checkout",info.default_branch ])
     subprocess.check_output(["git","checkout",info.current_branch ])
     version_pattern = re.compile(
-        rf"^{info.version_check_variable}.*?=.*?['\"](.*?)['\"]", flags=re.M
+        rf"^({info.version_check_variable}.*?=.*?['\"])(.*?)(['\"].*)$", flags=re.M
     )
     to_version = lambda cmd: pkg_resources.parse_version(
         version_pattern.findall(subprocess.check_output(cmd).decode())[0]
@@ -23,23 +23,18 @@ def run(info: _api.PackageInfo):
         ["git", "show", f"{info.default_branch}:{info.version_check_path}"]
     )
     current = to_version(["cat", info.version_check_path])
-    print(main,current)
+    next_version = re.sub(r"^(\d+\.\d+)\.\d*(.*)$", rf"\1.{main.micro+1}\2", str(main))
+    print(main,current,next_version)
     assert (
         main < current
     ), f"Version of current commit ({current}) has not been incremented (from {main})."
 
 
 def update_version():
-    path, variable, default_branch = sys.argv[1:4]
 
-    version_pattern = re.compile(
-        rf"^({variable}.*?=.*?[''\"])(.*?)([''\"].*)$", flags=re.M
-    )
-    to_version = lambda cmd: pkg_resources.parse_version(
-        version_pattern.findall(subprocess.check_output(cmd).decode())[0][1]
-    )
+
     main = to_version(["git", "show", f"{default_branch}:{path}"])
-    next_version = re.sub(r"^(\d+\.\d+)\.\d*(.*)$", rf"\1.{main.micro+1}\2", str(main))
+    
     with open(path, "r") as fp:
         text = fp.read()
         with open(path, "w") as fp:
